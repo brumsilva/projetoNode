eccf152b6f0ab296ef487599dc840abd7b14e5b8
Rafael Gomes <rafavgomes@yahoo.com>
2021-11-11 14:40:45 -0300

    IMG mapeado e mais algumas alterações

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 //RAFAEL
-
-// variávél para armazenar as funções do express (framework do node.js)
+// variável para armazenar as funções do express (framework do node.js)
 const express = require('express');
 //variável para armazenar as funções do módulo fs
 const fs = require('fs');
 //variável para armazenar o número da portado servidor
 const porta = 3000;
-
 //instanciando o express para a variável app
 const app = express();
-
+// Definindo uma rota com .use() para entregar os arquivos de imagem através do middleware express.static()
+app.use('/img', express.static('img'))
 //executando a função get através da variável app
 app.get('/', (req, res)=>{
     fs.readFile('index.html', (err, arquivo)=>{
@@ -18,7 +17,7 @@ app.get('/', (req, res)=>{
             console.log(err)
         }else{
             //se não ocorreu erro, exige o arquivo index.hml
-            res.set({'Content-Type': 'text/html'})
+            res.set({'Content-Type': 'html'})
             res.send(arquivo)
         }
     });
@@ -26,6 +25,19 @@ app.get('/', (req, res)=>{
 //essa é a segunda rota que chama produtos.html
 app.get('/produtos', (req, res)=>{
     fs.readFile('produtos.html', (err, arquivo) =>{
+        if(err){
+            console.log(err)
+        }else{
+            //se não ocorreu erro, exige o arquivo index.hml
+            res.set({'Content-Type': 'text/html'})
+            res.send(arquivo)
+        }
+    })
+})
+
+//essa é a terceira rota que chama carrinho.html
+app.get('/quemsomos', (req, res)=>{
+    fs.readFile('quemSomos.html', (err, arquivo) =>{
         if(err){
             console.log(err)
         }else{
@@ -48,19 +60,6 @@ app.get('/carrinho', (req, res)=>{
         }
     })
 })
-
-//essa é a terceira rota que chama carrinho.html
-app.get('/quemsomos', (req, res)=>{
-    fs.readFile('quemsomos.html', (err, arquivo) =>{
-        if(err){
-            console.log(err)
-        }else{
-            //se não ocorreu erro, exige o arquivo index.hml
-            res.set({'Content-Type': 'text/html'})
-            res.send(arquivo)
-        }
-    })
-})
-app.listen(porta,()=>{console.log('Servidor rodando')})
+app.listen(porta,()=>{console.log(`Servidor rodando na porta: ${porta}`)})
 
 //
